ee453085810c2cf16022fd6d279d914bccb3e9d0
pmtrojniak <patrycjatrojniak@gmail.com>
2025-03-01 21:31:05 +0100

    initial script

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const params = new URLSearchParams(window.location.search);
+    const productId = params.get('id') || 1;
+
+    document.getElementById("product-image").src = `images/kosz_placeholder.jpg`;
+    document.getElementById("product-title").textContent = `Produkt ${productId}`;
+    document.getElementById("product-description").textContent = `Opis produktu ${productId}...`;
+});
 
